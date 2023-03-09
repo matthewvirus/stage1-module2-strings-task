62e06f73eebef3a6f4572e748129702982c55e88
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-03-09 22:33:51 +0300

    Solved

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -20,6 +23,26 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<MethodSignature.Argument> argumentList = new ArrayList<>();
+        MethodSignature methodSignature;
+        String[] signatureArray = signatureString.split("[(%)]");
+        String[] mainSignatureArray = signatureArray[0].split(" ");
+        String[] methodArgumentsArray;
+        if (signatureArray.length == 2) {
+            methodArgumentsArray = signatureArray[1].split(", ");
+            for (String s : methodArgumentsArray) {
+                argumentList.add(new MethodSignature.Argument(s.split(" ")[0], s.split(" ")[1]));
+            }
+        }
+        methodSignature = new MethodSignature("", argumentList);
+        if (mainSignatureArray.length == 3) {
+            methodSignature.setAccessModifier(mainSignatureArray[0]);
+            methodSignature.setReturnType(mainSignatureArray[1]);
+            methodSignature.setMethodName(mainSignatureArray[2]);
+        } else {
+            methodSignature.setReturnType(mainSignatureArray[0]);
+            methodSignature.setMethodName(mainSignatureArray[1]);
+        }
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,23 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
      * @param source source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        for (String delimiter: delimiters) {
+            source = source.replace(delimiter, " ").trim();
+        }
+        String[] trimmedSource = source.split(" +");
+        return Arrays.asList(trimmedSource);
     }
 }
